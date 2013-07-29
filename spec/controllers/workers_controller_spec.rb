1f10f0699d5d1c7bd03872c86b383f97a3227371
Ryan <oloagentolo@gmail.com>
2013-07-28 19:01:07 -0500

    Added worker creation and validations

--- spec/controllers/workers_controller_spec.rb
+++ spec/controllers/workers_controller_spec.rb
@@ -26,4 +26,40 @@ describe WorkersController do
       response.should have_selector('title', :content => 'New worker')
     end
   end
+
+  describe "POST request for 'create'" do
+    describe 'failure' do
+      before(:each) do
+        @attr = { :first_name => '', :last_name => '' }
+      end
+
+      it 'should not create an invalid worker' do
+        lambda do
+          post :create, :worker => @attr
+        end.should_not change(Worker, :count)
+      end
+
+      it "should render the 'new' page" do
+        post :create, :worker => @attr
+        response.should render_template('new')
+      end
+    end
+
+    describe 'success' do
+      before(:each) do
+        @attr = { :last_name => 'Last', :first_name => 'First' }
+      end
+
+      it 'should create a worker' do
+        lambda do
+          post :create, :worker => @attr
+        end.should change(Worker, :count).by(1)
+      end
+
+      it 'should redirect to the workers page' do
+        post :create, :worker => @attr
+        response.should redirect_to(root_path)
+      end
+    end
+  end
 end
